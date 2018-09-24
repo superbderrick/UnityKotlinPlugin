bcedf346c5c2fca1b54fefd050cb7e25c73fc8ab
derrick <derrick.kang@nexstreaming.com>
2018-09-24 16:09:19 +0900

    completed basic triangle

--- Makingmeshs/Assets/ProceduleMesh.cs
+++ Makingmeshs/Assets/ProceduleMesh.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class ProceduleMesh : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Mesh mesh;
+
+    Vector3[] verticles;
+    int[] triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+    }
+    // Use this for initialization
+    void Start () {
+        MakeMeshData();
+        UpdateMesh();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    void MakeMeshData() {
+        verticles = new Vector3[]{new Vector3(0,0,0), new Vector3(0, 0, 1) , new Vector3(1, 0, 0) };
+
+        triangles = new int[] { 0, 1, 2 };
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+        mesh.vertices = verticles;
+
+        mesh.triangles = triangles;
+    }
 }
